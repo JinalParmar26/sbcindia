31053e2410c3d69ecf4f7fe2115f1ae680910af5
Jinal Parmar <Jinalparmar.8888@gmail.com>
2025-05-29 18:13:44 +0530

    fixes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,7 +43,6 @@ class Kernel extends HttpKernel
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class, // for SPA if needed
             'throttle:api',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
-            'approval:check' => \App\Http\Middleware\CheckUserApproval::class,
         ],
 
     ];
@@ -65,5 +64,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'approval.check' => \App\Http\Middleware\CheckUserApproval::class,
     ];
 }
