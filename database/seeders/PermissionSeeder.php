71a8cc5616f206aa61ef38b7625eddc9218e2628
anitrathod <anitrathod1991@gmail.com>
2025-08-10 17:56:17 +0530

    Add PermissionSeeder and update DatabaseSeeder to include permissions

---
+++ database/seeders/PermissionSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
+
+class PermissionSeeder extends Seeder
+{
+    public function run(): void
+    {
+        // First, clear cached permissions
+        app()[\Spatie\Permission\PermissionRegistrar::class]->forgetCachedPermissions();
+
+        // Create permissions
+        $permissions = [
+            'view users',
+            'create users',
+            'edit users',
+            'delete users',
+            'view orders',
+            'create orders',
+            'edit orders',
+            'delete orders',
+        ];
+
+        foreach ($permissions as $permission) {
+            Permission::firstOrCreate(['name' => $permission]);
+        }
+
+        // Create roles and assign permissions
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        $adminRole->givePermissionTo(Permission::all());
+
+        $userRole = Role::firstOrCreate(['name' => 'user']);
+        $userRole->givePermissionTo(['view orders']);
+
+        $managerRole = Role::firstOrCreate(['name' => 'manager']);
+        $managerRole->givePermissionTo([
+            'view users',
+            'view orders',
+            'edit orders',
+        ]);
+    }
+}
